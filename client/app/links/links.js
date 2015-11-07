215579ae8270ca040f0d482f0b30c123e5fddf62
alexmclean <alexmclean206@berkeley.edu>
2015-11-06 19:56:20 -0800

    Implemented a live updating search bar and also fixed issue where view count wasn't updating

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,6 +1,7 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+
+.controller('LinksController', function ($scope, $location, Links) {
   // Your code here
   // make links clickable, store links, increment count, and add links
   Links.getLinks ()
@@ -9,7 +10,8 @@ angular.module('shortly.links', [])
   });
 
   $scope.handleClick = function (ind) {
-    console.log(this.data[ind]);
-    this.data[ind].visits++;
+    console.log($location);
+    // $location.url('/api/links/' + this.data[ind].code);
+    window.location.href = "http://localhost:3000/api/links/" + $scope.data[ind].code;
   };
 });
